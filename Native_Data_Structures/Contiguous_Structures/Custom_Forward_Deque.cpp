a312f50e852030f53dcbdca96d1232526010611a
Shahzaib Khan <k190273@nu.edu.pk>
2021-08-20 17:41:09 +0500

    Update Custom_Forward_Deque.cpp

--- Native_Data_Structures/Contiguous_Structures/Custom_Forward_Deque.cpp
+++ Native_Data_Structures/Contiguous_Structures/Custom_Forward_Deque.cpp
@@ -1,59 +1,63 @@
 #include"Forward_Deque.hpp"
+#include"String.hpp"
 #include"Sorts.hpp"
 
 void basic_operations(){
-	Forward_Deque<int,8> list;//i.e. default ctor
-	Forward_Deque<int,8> list2(33,444);//i.e. (emplaced) fill ctor
-//	Forward_Deque<int,8> list3{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};//i.e. initializer-list ctor (C++11)
-	Forward_Deque<int,8> list4(list);//i.e. copy ctor
-//	Forward_Deque<int,8> list5(move(list));//i.e. move ctor (C++11)
+	const int CC=8; //i.e. chunk capacity
+	Forward_Deque<int,CC> list;//i.e. default ctor
+	Forward_Deque<int,CC> list2(33,444);//i.e. (emplaced) fill ctor
+	Forward_Deque<int,CC> list3{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};//i.e. initializer-list ctor
+	Forward_Deque<int,CC> list4(list);//i.e. copy ctor
+	Forward_Deque<int,CC> list5(move(list));//i.e. move ctor
 	
-	cout<<boolalpha<<(list<list2)<<' '<<(list==list2)<<'\n';//i.e. comparison operators
+	cout<<boolalpha<<(list<list2)<<' '<<(list2==list3)<<'\n';//i.e. comparison operators
 	
-//	for(auto& it:list3)//C++11
+//	for(auto& it:list3)//range-based loop
 //		cout<<it<<' ';
-	for(Forward_Deque<int,8>::iterator it=list2.begin();it!=list2.end();++it)//for both versions
+	for(Forward_Deque<int,CC>::iterator it=list3.begin();it!=list3.end();++it)//actual deduction for above range-based 'for' loop
 		cout<<*it<<' ';
 	cout<<'\n';
 	
-//	for(auto& it:list3.r_iterator())//C++11
+//	for(auto& it:list3.r_iterator())
 //		cout<<it<<' ';
-//	for(Forward_Deque<int,8>::reverse_iterator it=list2.rbegin();it!=list2.rend();++it)
+//	for(Forward_Deque<int,CC>::reverse_iterator it=list3.rbegin();it!=list3.rend();++it)
 //		cout<<*it<<' ';
 	
-//	for(Forward_Deque<int,8>::sf_iterator it=list3.sfbegin()+1;it<list3.sfend()-5;++it)//prints 2 to 15
+//	for(Forward_Deque<int,CC>::sf_iterator it=list3.sfbegin()+1;it<list3.sfend()-5;++it)//prints 2 to 15
 //		cout<<*it<<' ';
 	
-//	cout<<list2.size()<<' '<<list2.capacity()<<'\n'
-//		<<list2<<'\n';//i.e. uses stream operator
+//	cout<<list3.size()<<' '<<list3.capacity()<<'\n'
+//		<<list3<<'\n';//i.e. uses stream operator
 }
 
 void pointer_invalidation_test(){// Vector vs Forward_Deque
-//	Vector<string> s1(5,"lol");//i.e. invalidates pointers to elements
-	Forward_Deque<string,4> s1(5,"lol");//i.e. doesn't invalidate pointers
-	string* ptr=s1+3;
-	cout<<*ptr;
-	s1.push_back("haha");
-	cout<<*ptr;//i.e. gives error for pointing previously pointed element in Vector<string> container
+	String *ptr;
+
+//Note: To test both containers, uncomment & check below lines one at a time
+//	Vector<String> string_list(5,"lol"); string_list.pop_back();	
+	Forward_Deque<String,5> string_list(4,"lol");
+	
+	string_list.push_back("aabbccddeeff12345");
+	ptr=&string_list.back();	
+	
+	cout<<ptr<<' '<<string_list+4<<" ==> "<<(ptr==string_list+4?"same":"different")
+		<<", pointed string: "<<*ptr<<'\n';//i.e. before reallocation
+	
+	string_list.push_back("new string");
+	
+	cout<<ptr<<' '<<string_list+4<<" ==> "<<(ptr==string_list+4?"same":"different")
+		<<", pointed string: "<<*ptr<<'\n';//i.e. after reallocation
+	/*Result:
+		After reallocation, we have null pointed string in Vector.
+		Whereas in Forward_Deque, we have same pointed string.
+		Hence, Forward_Deque doesn't invalidates pointer & Vector does.
+	*/
 }
 
 void sort_by_iterators_test(){
-#if __cplusplus >= 201103L
 	Forward_Deque<int> fd{2,-32,12,3,6,5,8,1,9,2,-67,45,401,901,99,111};
-	//Note: try to avoid using initializer-list in projects as it performs one extra copy of each element
-#else
-	Forward_Deque<int> fd;
-	fd.reserve(7);
-	fd.push_back(2); fd.push_back(-32);
-	fd.push_back(12); fd.push_back(3);
-	fd.push_back(6); fd.push_back(5);
-	fd.push_back(8); fd.push_back(1);
-	fd.push_back(9); fd.push_back(2);
-	fd.push_back(-67); fd.push_back(45);
-	fd.push_back(401); fd.push_back(901);
-	fd.push_back(99); fd.push_back(111);
-#endif
-	
+	//Note: try to avoid using initializer-list in bigger projects as it performs one extra copy of each element
+
 	cout<<"BEFORE SORTING: "<<fd<<endl;
 	
 	//NOTE: only special forward & reverse iterators will work here
@@ -65,11 +69,12 @@ void sort_by_iterators_test(){
 	cout<<"AFTER SORTING: "<<fd<<endl;
 }
 
-struct heavy{//i.e. 1 heavy object == 10,000 bytes
-	char d1[2000];
-	int d2[2000];
-};
 void reallocation_speed_test(){// Vector vs STL::Deque vs Forward_Deque
+	struct heavy{//i.e. local struct (C++11)
+		char d1[2000];
+		int d2[2000];
+	};//Note: here, 1 heavy object == 10,000 bytes
+
 	Forward_Deque<heavy,10000> x;
 //	deque<heavy> x;//i.e. takes avg. 7.06 secs (NOTE: need to include deque header-file first for testing)
 //	Vector<heavy> x;//i.e. takes avg. 27.56 secs
@@ -80,7 +85,7 @@ void reallocation_speed_test(){// Vector vs STL::Deque vs Forward_Deque
 	
 	/*WARNING:
 	  	 I've performed this test according to my PC environment. you must have enough free RAM space,
-		 otherwise system will crash (i.e. memory allocation is set for 14GB RAM size)
+		 otherwise system will crash (i.e. memory allocation is set for approx. 14GB RAM size)
 	  SIDE-NOTES:
 	  1) STL::Deque has no template argument for taking chunk_size (due to some security rule).
 		 Instead, it decides the size at compile-time by comparing the type-size with some constant values.
@@ -100,4 +105,3 @@ int main(){
 //	reallocation_speed_test();
 	return 0;
 }
-
